a16dccb6e70d8d640992cf7c9a1c7d38d9117568
MiGray <molotoksergey@gmail.com>
2022-05-27 20:56:55 +0300

    delete error config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5322113931:AAFj4Bo-KfGDfE6HisSJeVV2Fppv5VnCEeg"
 APP_URL = "https://migraybot.herokuapp.com/" + BOT_TOKEN
-DB_URR = "postgres://atgxvvlcvvwabk:addd676bd3658083039ddefb3d5e101f3504d470661c07c12e75cce76986ff5b@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d9a44b8f22nbsa"
+DB_URI = "postgres://atgxvvlcvvwabk:addd676bd3658083039ddefb3d5e101f3504d470661c07c12e75cce76986ff5b@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d9a44b8f22nbsa"
