9389d178b6f06b7e97df6ba985e131e60d66c2c8
MiGray <molotoksergey@gmail.com>
2022-05-27 20:11:46 +0300

    delete error in main

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import logging
 from config import *
 from flask import Flask, request
 
-
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
-logger =telebot.logger
+logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
-@bot.message_handler(commandsd=["start"])
+
+@bot.message_handler(commands=["start"])
 def start(message):
     username = message.from_user.username
     bot.reply_to(message, f"Hello, {username}!")
+
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
@@ -22,7 +23,8 @@ def redirect_message():
     bot.process_new_updates([update])
     return "!", 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
