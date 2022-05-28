1d5beb62a1f828a88f1bcebf8010e6d6fc540b44
unknown <molotoksergey@gmail.com>
2022-05-28 14:58:41 +0300

    add Привет

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def update_message_count(user_id):
 def start(message):
     user_id = message.from_user.id
     username = message.from_user.username
-    bot.reply_to(message, f"Hello, {username}!")
+    bot.reply_to(message, f"Привет, {username}!")
 
     db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
     result = db_object.fetchone()
